4d3e3166af3d1500c98026d0fae348b42979666a
ponponumi <ponponumi@gmail.com>
2024-12-21 15:52:52 +0900

    名前を返すメソッドを作成

--- src/CustomizeSimpleNameGet.php
+++ src/CustomizeSimpleNameGet.php
@@ -10,4 +10,21 @@ class CustomizeSimpleNameGet
     {
         $this->themeName = $themeName;
     }
+
+    public function get(string $panelName, string $sectionName="", string $settingName=""): string
+    {
+        $result = $this->themeName . "_" . $panelName;
+
+        if($sectionName === ""){
+            return $result;
+        }
+
+        $result .= "_" . $sectionName;
+
+        if($settingName === ""){
+            return $result;
+        }
+
+        return $result . "_" . $settingName;
+    }
 }
