ba626604228ef11a85231dddae7ae0c2ed410c25
ponponumi <ponponumi@gmail.com>
2024-12-21 15:22:43 +0900

    セクションを追加するメソッドを作成

--- src/CustomizeOptionAdd.php
+++ src/CustomizeOptionAdd.php
@@ -6,6 +6,8 @@ class CustomizeOptionAdd
 {
     private object $wpCustom;
     private string $panelName;
+    private int $sectionPriority = 1;
+    private int $settingPriority = 1;
 
     public function __construct(object $wpCustom)
     {
@@ -16,5 +18,16 @@ class CustomizeOptionAdd
     {
         $this->panelName = $panelName;
         $this->wpCustom->add_panel($panelName, $option);
+    }
+
+    public function sectionSet(string $sectionName,array $option)
+    {
+        $option["panel"] = $this->panelName;
+        $option["priority"] = $this->sectionPriority;
+
+        $this->sectionPriority++;
+        $this->settingPriority = 1;
+
+        $this->wpCustom->add_section($sectionName, $option);
     }
 }
