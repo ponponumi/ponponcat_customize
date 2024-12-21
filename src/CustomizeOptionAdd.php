fc13787305f0344d3d63b2406296e0d224db1f7f
ponponumi <ponponumi@gmail.com>
2024-12-21 15:19:29 +0900

    カスタムインスタンスを追加

--- src/CustomizeOptionAdd.php
+++ src/CustomizeOptionAdd.php
@@ -4,5 +4,10 @@ namespace Ponponumi\PonponcatCustomize;
 
 class CustomizeOptionAdd
 {
-    //
+    private object $wpCustom;
+
+    public function __construct(object $wpCustom)
+    {
+        $this->wpCustom = $wpCustom;
+    }
 }
