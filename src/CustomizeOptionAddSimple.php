6616b638822ddb683f2e1c6dcd193ae4c2eeda74
ponponumi <ponponumi@gmail.com>
2024-12-21 15:40:56 +0900

    プロパティを追加

--- src/CustomizeOptionAddSimple.php
+++ src/CustomizeOptionAddSimple.php
@@ -4,5 +4,8 @@ namespace Ponponumi\PonponcatCustomize;
 
 class CustomizeOptionAddSimple
 {
-    //
+    public object $customAdd;
+    public string $themeName = "ponponcat";
+    public string $panelName = "";
+    public string $sectionName = "";
 }
