88f05dedbbd2df76d630b2438ce6117f9a4571b1
ponponumi <ponponumi@gmail.com>
2024-12-21 15:49:46 +0900

    コンストラクタを作成

--- src/CustomizeSimpleNameGet.php
+++ src/CustomizeSimpleNameGet.php
@@ -5,4 +5,9 @@ namespace Ponponumi\PonponcatCustomize;
 class CustomizeSimpleNameGet
 {
     public string $themeName = "ponponcat";
+
+    public function __construct(string $themeName = "ponponcat")
+    {
+        $this->themeName = $themeName;
+    }
 }
