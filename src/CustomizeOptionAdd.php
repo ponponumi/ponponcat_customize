fe3572dedbe1056039ea20b6e003ece4ad1260b4
ponponumi <ponponumi@gmail.com>
2024-12-21 15:20:46 +0900

    パネルを追加するメソッドを作成

--- src/CustomizeOptionAdd.php
+++ src/CustomizeOptionAdd.php
@@ -5,9 +5,16 @@ namespace Ponponumi\PonponcatCustomize;
 class CustomizeOptionAdd
 {
     private object $wpCustom;
+    private string $panelName;
 
     public function __construct(object $wpCustom)
     {
         $this->wpCustom = $wpCustom;
+    }
+
+    public function panelSet(string $panelName,array $option)
+    {
+        $this->panelName = $panelName;
+        $this->wpCustom->add_panel($panelName, $option);
     }
 }
