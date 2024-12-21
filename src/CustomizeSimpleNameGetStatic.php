4cf06a824e441e8d2f655835487ced7789e3a01a
ponponumi <ponponumi@gmail.com>
2024-12-21 18:23:07 +0900

    取得するメソッドを作成

--- src/CustomizeSimpleNameGetStatic.php
+++ src/CustomizeSimpleNameGetStatic.php
@@ -4,5 +4,20 @@ namespace Ponponumi\PonponcatCustomize;
 
 class CustomizeSimpleNameGetStatic
 {
-    //
+    public static function get(string $themeName, string $panelName, string $sectionName = "", string $settingName = ""): string
+    {
+        $result = $themeName . "_" . $panelName;
+
+        if($sectionName === ""){
+            return $result;
+        }
+
+        $result .= "_" . $sectionName;
+
+        if($settingName === ""){
+            return $result;
+        }
+
+        return $result . "_" . $settingName;
+    }
 }
